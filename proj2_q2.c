a776532053882d3d8c093ae5ac3983cad79a139d
Bryanong9440 <bongzenh@terpmail.umd.edu>
2024-10-13 18:44:05 -0400

    Create proj2_q2.c

---
+++ proj2_q2.c
@@ -0,0 +1,20 @@
+int main()
+{
+    int number;
+    printf("Please enter an integer: ");
+    scanf("%i", &number);
+    for(int i = 0; i < number; i++){
+        printf("*");
+    }
+    printf("\n");
+    for(int i = 0; i < number - 2; i++){
+        printf("*");
+        for(int j = 0; j < number - 2; j++){
+            printf(" ");
+        }
+        printf("*\n");
+    }
+    for(int i = 0; i < number; i++){
+        printf("*");
+    }
+}
