7df13bfb998d064ba4fb37682d0cd31e453d859b
Bryanong9440 <bongzenh@terpmail.umd.edu>
2024-10-14 15:17:08 -0400

    Update proj2_q4.c

--- proj2_q4.c
+++ proj2_q4.c
@@ -28,7 +28,7 @@ int main()
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 2; j++){
             printf("%i", array2[i][j]);
-            if(j = 0){
+            if(j == 0){
                 printf(" ");
             }
         }
