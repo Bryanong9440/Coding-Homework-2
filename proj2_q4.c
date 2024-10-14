7361db69b6f8be358615f0596101658d795a6572
Bryanong9440 <bongzenh@terpmail.umd.edu>
2024-10-14 15:10:49 -0400

    Update proj2_q4.c

--- proj2_q4.c
+++ proj2_q4.c
@@ -10,10 +10,13 @@ int main()
     printf("First Matrix entered.\n");
     for(int i = 0; i < 2; i++){
         for(int j = 0; j < 3; j++){
-            printf("%i ", array[i][j]);
+            printf("%i", array[i][j]);
+            if(j < 2){
+                printf(" ");
             }
-        printf("\n");
         }
+        printf("\n");
+    }
     int array2[3][2];
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 2; j++){
@@ -24,10 +27,13 @@ int main()
     printf("Second Matrix entered.\n");
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 2; j++){
-            printf("%i ", array2[i][j]);
+            printf("%i", array2[i][j]);
+            if(j < 1){
+                printf(" ");
             }
-        printf("\n");
         }
+        printf("\n");
+    }
     int sum;
     sum = array[0][0] * array2[0][0] + array[0][1] * array2[1][0] + array[0][2] * array2[2][0];
     printf("Result: \n");
